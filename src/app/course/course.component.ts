8e3e1e6ab3f378fef33fbb03e442edd975f784e5
Anastasia <fioB7@yandex.ru>
2023-07-06 20:20:30 +0500

    сделан hero главной курса

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent {
-
+  public progressPercent: number = 100;
+  public chapters: {id: number, isCompleted: boolean}[] = [
+    {
+      id: 0,
+      isCompleted: true
+    },
+    {
+      id: 1,
+      isCompleted: true
+    },
+    {
+      id: 2,
+      isCompleted: false
+    },
+    {
+      id: 3,
+      isCompleted: true
+    },
+    {
+      id: 4,
+      isCompleted: false
+    },
+  ];
 }
