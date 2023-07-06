65eef05dc1fdbbfb84e5ef579da69e011e3c1e87
Anastasia <fioB7@yandex.ru>
2023-07-06 14:13:16 +0500

    продолжение навигации

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {NavigationComponent} from "./home/navigation/navigation.component";
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
